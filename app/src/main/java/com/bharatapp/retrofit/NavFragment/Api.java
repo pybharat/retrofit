b3477a99691127dcaaf4e2742567f35cead10633
bharat <bharatsinghal004@gmail.com>
2021-09-28 08:18:36 +0530

    aa

--- app/src/main/java/com/bharatapp/retrofit/NavFragment/Api.java
+++ app/src/main/java/com/bharatapp/retrofit/NavFragment/Api.java
@@ -1,19 +1,19 @@
 package com.bharatapp.retrofit.NavFragment;
 
+import com.bharatapp.retrofit.ModelPost.RegisterPost;
 import com.bharatapp.retrofit.ModelResponse.RegisterResponse;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 
 public interface Api {
-    @FormUrlEncoded
+
     @POST("Auth/register")
     Call<RegisterResponse>register(
-            @Field("email")String email,
-            @Field("name")String name,
-            @Field("number")String contact,
-            @Field("password")String password
-    );
+            @Body RegisterPost registerPost
+            );
 }
